271b95ce4f3a60c5bea2c7ecaf6538aef6580d59
bohauhuang <nctu0511097@gmail.com>
2019-09-01 02:57:26 +0800

    0901 updated, add 2014-2015 ACM-ICPC NEERC

---
+++ 2019 Summer Training/Contest/2014-2015 ACM-ICPC, NEERC, Eastern Subregional Contest/A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long int f;
+	cin >> f;
+	if((12-f)*45 <= 240) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	
+	return 0;
+}
