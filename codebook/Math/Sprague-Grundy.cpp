4f81d88195bd093f729fbd065229027998cb2e48
FangFang <andy89923@gmail.com>
2019-11-15 20:49:35 +0800

    Update Sprague-Grundy.cpp

--- codebook/Math/Sprague-Grundy.cpp
+++ codebook/Math/Sprague-Grundy.cpp
@@ -15,3 +15,43 @@ int SG(A) {
   }
   return state[A]
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 110
+int sg[MAX], vis[MAX];
+
+int main() {
+    sg[1] = 0;
+    for (int i = 2; i <= 30; i++) {
+        memset(vis, 0, sizeof(vis));
+        for (int j = 1; j * 2 <= i; j++) vis[sg[i - j]] = 1;
+        for (int j = 0; ; j++) if (!vis[j]) {
+            sg[i] = j;
+            break;
+        }
+        cout << sg[i] << ' ';
+    }
+    return 0;
+}
+
+// 
+long long SG(long long x) {
+    return x % 2 == 0 ? x / 2: SG(x / 2);
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; 
+        long long a, v = 0;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> a;
+            v ^= SG(a);
+        }
+        cout << (v ? "YES" : "NO") << '\n';
+    }
+    return 0;
+}
