9c22026be509a65d986ae6964df7882cf9b15a62
FangFang <andy89923@gmail.com>
2020-10-08 00:14:55 +0800

    Add files via upload

---
+++ 2020 Taiwan Online Programming Contest (TOPC)/i.cpp
@@ -0,0 +1,12 @@
+/*input
+1 10 100 1000 
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << 56 * a + 24 * b + 14 * c + 6 * d << '\n';
+	return 0;
+}
